a13431410babac0b17ccdacff55f8c92e37aaaf5
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2024-11-18 02:34:26 +0000

    first commit

---
+++ ImageEncoder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void mostraManual();
+
+int main(int argc, char *argv[])
+{
+
+}
+
+void mostraManual()
+{
+    //Função que mostra o help para orientações de utilização do programa
+} 
