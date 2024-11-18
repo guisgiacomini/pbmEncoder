5876da691dab2a3607bc497df6d05fe905c462b2
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2024-11-18 11:37:41 +0000

    commit

--- ImageEncoder.c
+++ ImageEncoder.c
@@ -4,7 +4,10 @@ void mostraManual();
 
 int main(int argc, char *argv[])
 {
+    FILE *arquivo = fopen("fodase.txt", "w");
+    fprintf(arquivo, "fodase");
 
+    return 0;
 }
 
 void mostraManual()
