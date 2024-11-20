d52c3aa9e5e0680965c4567e8c200749da73d304
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2024-11-20 03:45:07 +0000

    mensagem help

--- ImageEncoder.c
+++ ImageEncoder.c
@@ -1,16 +1,25 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
-void mostraManual();
+void help();
 
 int main(int argc, char *argv[])
 {
-    FILE *arquivo = fopen("fodase.txt", "w");
-    fprintf(arquivo, "fodase");
+    help();
 
     return 0;
 }
 
-void mostraManual()
+void help()
 {
     //Função que mostra o help para orientações de utilização do programa
+    printf("Uso: ./ImageEncoder [-? | -m | -f ARQ]\n");
+    printf("Codifica imagens binárias dadas em arquivos PBM ou por dados informados manualmente.");
+    printf("Argumentos:\n");
+    printf("-?, --help : apresenta essa orientação na tela.\n");
+    printf("-m, --manual: ativa o modo de entrada manual, em que o usuário fornece todos os dados da imagem informando-os através do teclado.\n");
+    printf("-f, --file: considera a imagem representada no arquivo PBM (Portable bitmap).\n");
+
+    return;
 } 
